bee6a6332373dc8c2f6e0410e82f10298405a709
umairsadiq88 <umairsadiq2010@gmail.com>
2020-04-27 01:45:09 +0500

    HTML Excercises

---
+++ 2nd.js
@@ -0,0 +1,51 @@
+document.write("<b>Hello World</b>"); //here is comment
+
+document.write(
+  "<i>Hello World Again..</i>"
+); /*here is multiline comment
+hello again comment*/
+
+//variables/
+var A = "Hello World";
+document.write(A);
+
+let B = "Hello Umair";
+document.write(B);
+
+const C = "HI Umair";
+document.write(C);
+
+//Data Type//
+
+/* 
+var x = "hello world"; ======== string
+var x = 25;======== Number
+var x = true; ======== Boolean
+var x = ["HTML", "CSS","JS"]; ======== Array
+var x = {first:"jane", last:"Doe"}; ======== Object
+var x = null; ======== Null
+var x ;================= undefined */
+
+var X = "welcome to javascript";
+
+document.write(X);
+document.write("<br>");
+document.write(typeof X);
+
+/* JavaScript Arithmetic Operators
+
++ addition
+- subtraction
+* multiplication
+** Exponentitation
+/ Division
+% Modulus (Remainder)
+++ Increment
+-- Decrement
+*/
+
+var a = 100;
+var b = 200;
+var c = a - b;
+
+document.write(c);
